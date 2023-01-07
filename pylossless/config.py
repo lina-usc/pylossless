33101d6205a8b11705cd1df2889079b94696f6c2
Christian O'Reilly <christian.oreilly@gmail.com>
2023-01-07 17:19:39 -0500

    Refactoring config.

--- pylossless/config.py
+++ pylossless/config.py
@@ -6,25 +6,27 @@ DEFAULT_CONFIG_PATH = (Path(__file__).parent.parent /
                        "assets" / "ll_default_config.yaml")
 
 
-def save_config(init_variables, file_name='init_variables.yaml'):
+def save_config(init_variables=None, path_in=None, id_=None, run=None, 
+                session=None, file_name='init_variables.yaml'):
+
+    if init_variables is None:
+        init_variables = {}
+    if path_in is not None:
+        init_variables.update({"path_in": path_in})
+    if id_ is not None:
+        init_variables.update({"id": id_})
+    if run is not None:
+        init_variables.update({"run": run})
+    if session is not None:
+        init_variables.update({"session": session})
+    
     with open(file_name, "w") as init_variables_file:
         yaml.dump(init_variables, init_variables_file,
                   indent=4, sort_keys=True)
 
-
 def read_config(file_name):
     with open(file_name, "r") as init_variables_file:
         return yaml.safe_load(init_variables_file)
-
-
-def create_init_variables_json(path_in, id_, run, session):
-    init_variables = {'path_in': path_in,
-                      'id': id_,
-                      'run': run,
-                      'session': session
-                      }
-    save_config(init_variables)
-
 
 def get_default_config():
     return read_config(DEFAULT_CONFIG_PATH)
