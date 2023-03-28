f36209e3d75af1afc513465e81d1935f79f4dc66
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2023-03-28 14:53:05 -0400

    TEST: add test for flag_ch_bridge Forced ch 15 and 16 to be identical. test that they are flagged by flag_ch_bridge. ch 27 is also being flag by flag_ch_bridge.

--- tests/test_simulated.py
+++ tests/test_simulated.py
@@ -35,6 +35,7 @@ n_dipoles = 4  # number of dipoles to create
 epoch_duration = 2.  # duration of each epoch/event
 n = 0  # harmonic number
 rng = np.random.RandomState(0)  # random state (make reproducible)
+np.random.seed(0)
 
 
 def data_fun(times):
@@ -61,6 +62,14 @@ stc = simulate_sparse_stc(src, n_dipoles=n_dipoles, times=times,
 raw_sim = simulate_raw(raw.info, [stc] * 10, forward=fwd, verbose=True)
 raw_sim.pick_types(eeg=True)
 
+# Save Info and Montage for later re-use
+montage = raw_sim.get_montage()
+raw_sim.set_montage(montage)
+info = mne.create_info(ch_names=raw_sim.ch_names,
+                       sfreq=raw_sim.info['sfreq'],
+                       ch_types=raw_sim.get_channel_types())
+
+
 # MAKE A VERY NOISY TIME PERIOD
 raw_selection1 = raw_sim.copy().crop(tmin=0, tmax=2, include_tmax=False)
 raw_selection2 = raw_sim.copy().crop(tmin=2, tmax=3, include_tmax=False)
@@ -76,23 +85,26 @@ raw_selection1.append([raw_selection2, raw_selection3])
 raw_selection1.set_annotations(None)
 raw_sim = raw_selection1
 
-# MAKE SOME VERY NOISY CHANNELS
-cov = make_ad_hoc_cov(raw_sim.info)
+# ADD INITIAL/REASONABLE AMOUNT OF NOISE FOR REST OF TIMES
+cov = make_ad_hoc_cov(raw_sim.info, std=dict(eeg=0.0000003))
 add_noise(raw_sim, cov, iir_filter=[0.2, -0.2, 0.04], random_state=rng)
 
-make_these_noisy = ['EEG 001', 'EEG 005', 'EEG 009']
+# MAKE SOME VERY NOISY CHANNELS
+make_these_noisy = ['EEG 001', 'EEG 005']
 cov_noisy = make_ad_hoc_cov(raw_sim.copy().pick(make_these_noisy).info,
                             std=dict(eeg=.000002))
 add_noise(raw_sim, cov_noisy, iir_filter=[0.2, -0.2, 0.04], random_state=rng)
 
 # MAKE LESS NOISY CHANNELS
-make_these_noisy = ['EEG 015', 'EEG 016']
+make_these_noisy = ['EEG 009', 'EEG 011']
 
-raw_selection1 = raw_sim.copy().crop(tmin=0, tmax=8, include_tmax=False)
-raw_selection2 = raw_sim.copy().crop(tmin=8, tmax=19.994505956825666)
+# Make them noisy for about 25% of time
+raw_selection1 = raw_sim.copy().crop(tmin=0, tmax=7, include_tmax=False)
+raw_selection2 = raw_sim.copy().crop(tmin=7, tmax=19.994505956825666)
 
-cov_less_noisy = make_ad_hoc_cov(raw_selection1.copy().pick(make_these_noisy).info,
-                                 std=dict(eeg=.0000008))
+this_info = raw_selection1.copy().pick(make_these_noisy).info
+cov_less_noisy = make_ad_hoc_cov(this_info,
+                                 std=dict(eeg=.0000009))
 add_noise(raw_selection1,
           cov_less_noisy,
           iir_filter=[0.2, -0.2, 0.04],
@@ -101,6 +113,23 @@ raw_selection1.append([raw_selection2])
 raw_selection1.set_annotations(None)
 raw_sim = raw_selection1
 
+# MAKE BRIDGED CHANNELS
+data = raw_sim.get_data()  # ch x times
+data[15, :] = data[14, :]  # duplicate the signal
+
+# Try to make chs 27 and 41 slightly more random
+# 27 will still be picked up by flag_ch_bridge
+# which seems to be very sensitive..
+ch_27 = data[26, :5000]  # just add randomness to half the signal..
+ch_41 = data[40, 5000:]
+data[26, :5000] = ch_27 + np.random.normal(0, .000002, len(ch_27))
+data[40, 5000:] = ch_41 + np.random.normal(0, .0000015, len(ch_41))
+# make new raw out of data
+raw_sim = mne.io.RawArray(data, info)
+# Re-set the montage
+raw_sim.set_montage(montage)
+
+# 26 40
 # LOAD DEFAULT CONFIG
 config = ll.config.Config()
 config.load_default()
@@ -117,9 +146,9 @@ def test_simulated_raw(pipeline):
     # RUN FLAG OUTLIER CHS
     pipeline.flag_outlier_chs()
     assert np.array_equal(pipeline.flagged_chs['outliers'],
-                          ['EEG 001', 'EEG 005', 'EEG 009'])
+                          ['EEG 001', 'EEG 005'])
     assert np.array_equal(pipeline.flagged_chs['manual'],
-                          ['EEG 001', 'EEG 005', 'EEG 009'])
+                          ['EEG 001', 'EEG 005'])
 
     pipeline.flag_ch_sd_epoch()
     assert np.array_equal(pipeline.flagged_epochs['ch_sd'],
@@ -128,11 +157,20 @@ def test_simulated_raw(pipeline):
     # RUN FLAG_CH_SD
     pipeline.flag_ch_sd_ch()
     assert np.array_equal(pipeline.flagged_chs['ch_sd'],
-                          ['EEG 015', 'EEG 016'])
+                          ['EEG 009', 'EEG 011'])
     assert np.array_equal(pipeline.flagged_chs['manual'],
                           ['EEG 001', 'EEG 005', 'EEG 009',
-                           'EEG 015', 'EEG 016'])
+                           'EEG 011'])
+
+    # RUN FLAG_CH_BRIDGE
+    data_r_ch = pipeline.flag_ch_low_r()
+    pipeline.flag_ch_bridge(data_r_ch)
+    assert np.array_equal(pipeline.flagged_chs['bridge'],
+                          ['EEG 015', 'EEG 016', 'EEG 027'])
+    assert np.array_equal(pipeline.flagged_chs['manual'],
+                          ['EEG 001', 'EEG 005', 'EEG 009',
+                           'EEG 011', 'EEG 015', 'EEG 016', 'EEG 027'])
 
     # Delete temp config file
     tmp_config_fname = Path(pipeline.config_fname).absolute()
-    tmp_config_fname.unlink()
+    tmp_config_fname.unlink()  # delete config file
