20596889127de3350dcace3adc7e4e2ad161dc13
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2023-05-06 12:45:56 -0400

    ENH: Expose some run_server vars from CLI

--- pylossless/dash/pylossless_qc.py
+++ pylossless/dash/pylossless_qc.py
@@ -10,11 +10,12 @@ from pylossless.dash.app import get_app
 desc = 'Launch QCR dashboard with optional directory and filename arguments.'
 
 
-def launch_dash_app(directory=None, filepath=None, disable_buttons=False):
+def launch_dash_app(directory=None, filepath=None, disable_buttons=False,
+                    debug=False, host='127.0.0.1', port=8050):
     """Launch dashboard."""
     app = get_app(fpath=filepath, project_root=directory,
                   disable_buttons=disable_buttons)
-    app.run_server(debug=True)
+    app.run_server(debug=debug, host=host, port=port)
 
 
 def main():
@@ -26,8 +27,13 @@ def main():
     parser.add_argument('--filepath', help='path to the EDF file to load')
     parser.add_argument('--disable_buttons', action='store_true',
                         help=disable_button_help)
+    parser.add_argument('debug', action='store_True',
+                        help='If set, run application in debug mode')
+    parser.add_argument('--host', help='host IP used to serve application')
+    parser.add_argument('--port', help='port used to serve application')
     args = parser.parse_args()
-    launch_dash_app(args.directory, args.filepath, args.disable_buttons)
+    launch_dash_app(args.directory, args.filepath, args.disable_buttons,
+                    args.debug, args.host, args.port)
 
 
 if __name__ == '__main__':
