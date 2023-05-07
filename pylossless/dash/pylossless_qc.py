5f223272969814df89e47a436fc3c57a584319e7
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-05-07 11:43:00 -0400

    ENH: Expose gunicorn CLI timeout arg to pylossless_qc CLI

--- pylossless/dash/pylossless_qc.py
+++ pylossless/dash/pylossless_qc.py
@@ -11,13 +11,15 @@ desc = 'Launch QCR dashboard with optional directory and filename arguments.'
 
 
 def launch_dash_app(directory=None, filepath=None, disable_buttons=False,
-                    debug=False, host='127.0.0.1', port=None,
+                    debug=False, host='127.0.0.1', port=None, timeout=30,
                     deployment=False):
     """Launch dashboard."""
     if not port:
         port = 8050
     if not host:
         host = '127.0.0.1'
+    if not timeout:
+        timeout = 30
     app = get_app(fpath=filepath, project_root=directory,
                   disable_buttons=disable_buttons)
     if not deployment:
@@ -27,6 +29,7 @@ def launch_dash_app(directory=None, filepath=None, disable_buttons=False,
         log = 'debug' if debug else 'INFO'  # '--chdir', './pylossless/dash',
         cmd = ['gunicorn', 'pylossless.dash.app:server',
                '-b', f"{host}:{port}", f'--log-level={log}',
+               '-t', f"{timeout}",
                '--env', f'fpath={filepath}',
                '--env', f'disable_buttons={disable_buttons}']
         subprocess.Popen(cmd)
@@ -38,6 +41,8 @@ def main(deployment=False):
                            ' deactivated')
     port_help = ('port used to serve application. If not set, defaults to 8050'
                  )
+    timeout_help = {'set the timeout in gunicorn for the deployment server.'
+                    ' default is 30 seconds'}
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--directory', help='path to the project folder')
     parser.add_argument('--filepath', help='path to the EDF file to load')
@@ -48,10 +53,11 @@ def main(deployment=False):
     parser.add_argument('--host', help='host IP used to serve application')
     parser.add_argument('--port', help=port_help)
     parser.add_argument('--deployment', action='store_true', help="launch in deployment server.")
+    parser.add_argument('--timeout', help=timeout_help)
     args = parser.parse_args()
     launch_dash_app(args.directory, args.filepath, args.disable_buttons,
                     args.debug, args.host, args.port,
-                    deployment=args.deployment)
+                    deployment=args.deployment, timeout=args.timeout)
 
 
 if __name__ == '__main__':
