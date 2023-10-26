8119eb7d322b1f60ff83eb528e73b90d550d21de
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-10-26 15:28:20 -0400

    DOC: Switch sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "shibuya"
 html_static_path = ["_static"]
 html_theme_options = {
     "logo": {
