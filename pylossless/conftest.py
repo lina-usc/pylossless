ed18a7f6320e36d561e323a4b5655b0886b7785e
Scott Huberty <scott.huberty@mail.mcgill.ca>
2024-01-19 10:34:26 -0800

    MAINT, TST: Bump versions, ignore mne dep warning in tests
    - Brings our min versions in line with MNE
    - Sets min version of mne_bids to latet ( 0.14), necessary because they just had to change their API to be in line with a change in MNE.

--- pylossless/conftest.py
+++ pylossless/conftest.py
@@ -13,9 +13,6 @@ from pylossless.datasets import load_openneuro_bids
 
 import pytest
 
-# TODO: Remove this once mne 1.7 is released
-pytest.mark.filterwarnings("ignore:The current default of copy")
-
 
 @pytest.fixture(scope="session")
 def pipeline_fixture():
