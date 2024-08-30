fec43cf5d4abe5f399f259e7ef2a6407c948b217
Scott Huberty <seh33@uw.edu>
2024-08-30 16:27:02 -0700

    FIX: update init

--- pylossless/utils/__init__.py
+++ pylossless/utils/__init__.py
@@ -1,2 +1,3 @@
 from ._utils import _icalabel_to_data_frame, _report_flagged_epochs
+from .check import import_optional_dependency
 from .html import _get_ics, _sum_flagged_times, _create_html_details
