966679ff95c19a6501c2c9184820f7484f19fe34
Christian O'Reilly <christian.oreilly@gmail.com>
2024-11-09 15:38:48 -0500

    Updating the default config to be compatible with adult recordings.

--- pylossless/config/config.py
+++ pylossless/config/config.py
@@ -15,7 +15,7 @@ class ConfigMixin(dict):
     """Base configuration file class for pipeline procedures."""
 
     DEFAULT_CONFIG_PATH = (
-        Path(__file__).parent.parent / "assets" / "ll_default_config.yaml"
+        Path(__file__).parent.parent / "assets"
     )
 
     def read(self, file_name):
@@ -49,13 +49,19 @@ class ConfigMixin(dict):
 
 
 class Config(ConfigMixin):
-    """Representation of configuration file for running the pipeline."""
+    """Representation of configuration file for running the pipeline.
 
-    DEFAULT_CONFIG_PATH = (
-        Path(__file__).parent.parent / "assets" / "ll_default_config.yaml"
-    )
+        Parameters
+        ----------
+        kind : str | pathlib.Path
+            Can be either 'adults' or 'infants'. Default to 'adults'.
+    """
 
-    def load_default(self):
+    def load_default(self, kind="adults"):
         """Get the default pylossless config file."""
-        self.read(Config.DEFAULT_CONFIG_PATH)
+
+        path = Config.DEFAULT_CONFIG_PATH / f"ll_default_config_{kind}.yaml"
+        if not path.exists():
+            raise ValueError(f"No default configuration for kind '{kind}'.")
+        self.read(path)
         return self
