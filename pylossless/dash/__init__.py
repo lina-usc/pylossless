659e349a99d723eb6dc9620e726de0275c45a559
Zephanae Liis <143438674+iofeii@users.noreply.github.com>
2025-05-07 11:16:09 -0400

    Update __init__.py with proposed colors

--- pylossless/dash/__init__.py
+++ pylossless/dash/__init__.py
@@ -7,17 +7,28 @@ if not check_version("mne_icalabel", "0.5.0"):
         "Please upgrade mne_icalabel."
     )
 IC_COLORS = [
-    "#2c2c2c",
-    "#003e83",
-    "cyan",
-    "goldenrod",
-    "magenta",
-    "#b08699",
-    "#96bfe6",
-    "brown",
-    "yellowgreen",
-    "burlywood",
-    "plum",
-]
+# New Colors
+    "#000000", # BRAIN, Black
+    "#0173b2", # Muscle, Dark Blue
+    "#cc78bc", # EOG, Purple
+    "#de8f05", # ECG, Orange
+    "#56b4e9", # LINE NOISE, Cyan
+    "#ca9161", # CHANNEL NOISE, Brown
+    "#029e73", # OTHER, Green
+    "#d55e00",  # Unclassified, Reddish-Brown
+    "#fbafe4",  # Unclassified, Bright Pink
+    "#ece1334",  # Unclassified, Yellow
+
+""" Original Colors
+    "#2c2c2c",  # Brain, Dark Gray
+    "#cc78bc",  # EOG, Pink
+    "#d55e00",  # ECG, Red
+    "#029e73",  # Line Noise, Green
+    "#de8f05",  # Channel Noise, Orange
+    "#0173b2",  # Other, Blue
+    "#56b4e9",  # Unclassified, Light Blue
+    "#ece133",  # Unclassified, Yellow
+    "#949494",  # Unclassified, Gray
+"""
 
 ic_label_cmap = dict(zip(iclabel_config.ICA_LABELS_TO_MNE.values(), IC_COLORS))
